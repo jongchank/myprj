71d83ee9196c2d005548234524aa95e682eecdd7
Jong-Chan Kim <jongchank@gmail.com>
2024-06-27 16:35:49 +0900

    Modify prompt messages

--- cal_main.c
+++ cal_main.c
@@ -7,10 +7,10 @@ int main(int argc, char *argv[])
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
 
-    printf("=> %d + %d = %d\n", a, b, add(a, b));
-    printf("=> %d - %d = %d\n", a, b, sub(a, b));
-    printf("=> %d * %d = %d\n", a, b, mul(a, b));
-    printf("=> %d / %d = %d\n", a, b, dur(a, b));
+    printf("===> %d + %d = %d\n", a, b, add(a, b));
+    printf("===> %d - %d = %d\n", a, b, sub(a, b));
+    printf("===> %d * %d = %d\n", a, b, mul(a, b));
+    printf("===> %d / %d = %d\n", a, b, dur(a, b));
 
     return 0;
 }
